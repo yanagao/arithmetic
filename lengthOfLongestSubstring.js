650d76228baf4d29f1637a954ae8d86d48d5b596
yanagao <yanagao@tencent.com>
2021-05-20 16:25:53 +0800

    update

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -3,9 +3,20 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    let num = 0;
-    for(let i = 0; i < s.length; i++) {
-        
+ var lengthOfLongestSubstring = function(s) {
+    const occ = new Set();
+    let n = s.length;
+    let rk = -1;
+    let ans = 0;
+    for(let i = 0; i < n; i++) {
+        if(i !== 0) {
+            occ.delete(s.charAt(i - 1));
+        }
+        while(rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
+            occ.add(s.charAt(rk + 1));
+            ++rk;
+        }
+        ans = Math.max(ans, rk - i + 1);
     }
+    return ans;
 };
