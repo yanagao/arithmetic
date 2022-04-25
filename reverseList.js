e8c66cb771c4c8617cfa33103b8c49ffa80fc351
yanagao <yanagao@tencent.com>
2022-04-25 20:13:37 +0800

    update

--- reverseList.js
+++ reverseList.js
@@ -13,6 +13,14 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var reverseList = function(head) {
-
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    while(curr) {
+        const next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
 };
