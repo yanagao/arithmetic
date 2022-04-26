05db54d08f94f2fef104586693e2651eeaf86512
yanagao <yanagao@tencent.com>
2022-04-26 19:07:44 +0800

    update

---
+++ maxPathSum.js
@@ -0,0 +1,38 @@
+/**
+ * 124. 二叉树中的最大路径和
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+ * 路径和 是路径中各节点值的总和。
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    let maxSum = -Infinity;
+    const dfs = (root) => {
+        if (root === null) return 0;
+        const left = dfs(root.left);
+        const right = dfs(root.right);
+        
+        const currMaxSum = left + root.val + right;
+        maxSum = Math.max(maxSum, currMaxSum);
+
+        const outputMaxSum = root.val + Math.max(0, left, right);
+        return outputMaxSum < 0 ? 0 : outputMaxSum;
+    }
+
+    dfs(root);
+    return maxSum;
+};
